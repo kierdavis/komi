f607f7a687a0ca93bc42f3caebf3f806e7f29ea8
Kier Davis <me@kierdavis.com>
2020-12-07 23:41:25 +0000

    Include klog in bootstrap

--- bootstrap.lua
+++ bootstrap.lua
@@ -1,3 +1,22 @@
+local function _klog()
+  local path = "/var/log/komi.log"
+  local filesystem = require("filesystem")
+  local parent = filesystem.path(path)
+  if not filesystem.isDirectory(parent) then
+    filesystem.makeDirectory(parent)
+  end
+  
+  return function(msg)
+    local f, err = io.open(path, "a")
+    if not f then
+      error(err)
+    end
+    f:write(tostring(msg) .. "\n")
+    f:close()
+  end
+end  -- local function _klog
+package.loaded.klog = _klog()
+
 local function _knet()
   local knet = {}
   
